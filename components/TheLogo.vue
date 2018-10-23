929dcbc14bb875babdea87c6c9c09a97c38962d5
soichiro-nitta <soic1226@gmail.com>
2018-10-23 17:50:26 +0900

    add logo animation

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -3,15 +3,25 @@
     to="/"
     class="TheLogo"
   >
-    <img src="/svg/logo.svg">
+    <img
+      ref="logo"
+      src="/svg/logo.svg"
+    >
   </nuxt-link>
 </template>
 
 <script>
-// import { TweenMax, Expo } from 'gsap'
+import { TweenMax, Expo } from 'gsap'
 
 export default {
-  mounted() {}
+  mounted() {
+    requestAnimationFrame(() => {
+      TweenMax.to(this.$refs.logo, 1, {
+        marginTop: 0,
+        ease: Expo.easeOut
+      })
+    })
+  }
 }
 </script>
 
@@ -21,9 +31,15 @@ export default {
   position: fixed;
   top: 30px;
   left: 30px;
+  width: 40px;
+  height: 40px;
+  overflow: hidden;
+  border-radius: 50%;
   img {
-    width: 40px;
-    height: 40px;
+    margin-top: 100%;
+    padding: 5px;
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
