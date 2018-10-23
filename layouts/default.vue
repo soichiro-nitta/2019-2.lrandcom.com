1016ed45555e711f93ef3faa2a69d9933f9397c0
soichiro-nitta <soic1226@gmail.com>
2018-10-23 15:41:04 +0900

    update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,72 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="default">
+    <!-- <TheBackground/> -->
+    <!-- <TheOpening/> -->
+    <div id="scrollArea">
+      <nuxt/>
+    </div>
+    <!-- <TheNavigation/> -->
+    <!-- <TheLoading v-if="!loadingComplete"/> -->
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+// import { mapGetters } from 'vuex'
+// import TheBackground from '~/components/TheBackground'
+// import TheNavigation from '~/components/TheNavigation'
+// import TheOpening from '~/components/TheOpening'
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+export default {
+  // components: {
+  //   TheBackground,
+  //   TheNavigation,
+  //   TheOpening
+  // },
+  // computed: {
+  //   ...mapGetters({
+  //     loadingComplete: 'loadingComplete'
+  //   })
+  // },
+  mounted() {
+    if (!this.$device.isMobile) {
+      this.$nuxt.error({
+        statusCode: 777,
+        message: 'Desktop is not implemented yet'
+      })
+    }
+    console.log('this is staging')
+    // const progress = document.getElementsByClassName('nuxt-progress')[0]
+    // progress.style.height = this.$device.isMobile ? '10px' : '20px'
+    // progress.style.opacity = 1
+    // const userAgent = window.navigator.userAgent.toLowerCase()
+    // if (userAgent.indexOf('msie') !== -1 || userAgent.indexOf('trident') !== -1 || userAgent.indexOf('edge') !== -1) {
+    //   this.$router.replace({ path: 'sorry' })
+    // }
+  }
 }
+</script>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+<style lang="scss" scoped>
+.default {
+  position: relative;
+  margin: 5px;
+  width: calc(100% - 10px);
+  height: calc(100% - 10px);
+  overflow: hidden;
+  .wrap {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  #scrollArea {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling: touch;
+  }
+  // #scrollArea > div {
+  // }
 }
 </style>
