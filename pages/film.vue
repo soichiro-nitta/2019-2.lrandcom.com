870900878c9b7c29c1b35695d9cf8233f5c3ab1f
soichiro-nitta <soic1226@gmail.com>
2018-10-24 16:44:11 +0900

    wip

---
+++ pages/film.vue
@@ -0,0 +1,41 @@
+<template>
+  <Film v-if="opening"/>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Film from '~/components/Film'
+
+export default {
+  components: {
+    Film
+  },
+  computed: {
+    ...mapGetters({
+      opening: 'opening'
+    })
+  },
+  mounted() {
+    document.getElementById('scrollArea').scrollTop = 0
+  },
+  head() {
+    return {
+      title: '映像制作について',
+      meta: [
+        {
+          property: 'og:title',
+          content: '映像制作について | リーディング＆カンパニー株式会社'
+        },
+        {
+          property: 'og:url',
+          content: 'https://lrandcom.netlify.com/film'
+        },
+        {
+          property: 'twitter:title',
+          content: '映像制作について | リーディング＆カンパニー株式会社'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/website.vue
@@ -0,0 +1,41 @@
+<template>
+  <Website v-if="opening"/>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Website from '~/components/Website'
+
+export default {
+  components: {
+    Website
+  },
+  computed: {
+    ...mapGetters({
+      opening: 'opening'
+    })
+  },
+  mounted() {
+    document.getElementById('scrollArea').scrollTop = 0
+  },
+  head() {
+    return {
+      title: 'サイト制作について',
+      meta: [
+        {
+          property: 'og:title',
+          content: 'サイト制作について | リーディング＆カンパニー株式会社'
+        },
+        {
+          property: 'og:url',
+          content: 'https://lrandcom.netlify.com/website'
+        },
+        {
+          property: 'twitter:title',
+          content: 'サイト制作について | リーディング＆カンパニー株式会社'
+        }
+      ]
+    }
+  }
+}
+</script>
