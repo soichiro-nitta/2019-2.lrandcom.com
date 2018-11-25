8606893b4f82c1aa0f878f580120e23edcdee836
soichiro-nitta <soic1226@gmail.com>
2018-11-25 12:08:42 +0900

    add articles page

---
+++ pages/articles.vue
@@ -0,0 +1,41 @@
+<template>
+  <Page v-if="opening"/>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Page from '~/components/pages/contents/Page'
+
+export default {
+  components: {
+    Page
+  },
+  computed: {
+    ...mapGetters({
+      opening: 'opening'
+    })
+  },
+  mounted() {
+    document.getElementById('scrollArea').scrollTop = 0
+  },
+  head() {
+    return {
+      title: '記事制作について',
+      meta: [
+        {
+          property: 'og:title',
+          content: '記事制作について | リーディング＆カンパニー株式会社'
+        },
+        {
+          property: 'og:url',
+          content: 'https://lrandcom.netlify.com/contents'
+        },
+        {
+          property: 'twitter:title',
+          content: '記事制作について | リーディング＆カンパニー株式会社'
+        }
+      ]
+    }
+  }
+}
+</script>
