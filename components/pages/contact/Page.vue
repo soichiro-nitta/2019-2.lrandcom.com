d591682aa8949a9264ca233e10ebedcd0e86bcee
soichiro-nitta <soic1226@gmail.com>
2019-01-11 16:10:13 +0900

    add contact page

--- components/pages/contact/Page.vue
+++ components/pages/contact/Page.vue
@@ -4,7 +4,10 @@
       <div class="head">お問い合わせ</div>
       <div class="company">LEADING & COMPANY</div>
       <div class="text">メディア構築、コンテンツ作成をご希望の企業様から、面白いコンテンツを作ってみたいライターやクリエイターさん、そして、ただオフィスに遊びに来たいという変わり者の方まで、お気軽に下記のメールアドレスからご連絡下さい。</div>
-      <div class="email">HELLO@LRANDCOM.COM</div>
+      <ButtonNew
+        to="mailto:hello@lrandcom.com"
+        text="HELLO@LRANDCOM.COM"
+      />
     </div>
   </div>
 </template>
@@ -12,8 +15,12 @@
 <script>
 import URL from '~/assets/data/url.json'
 import { TweenMax, Expo } from 'gsap'
+import ButtonNew from '~/components/base/ButtonNew'
 
 export default {
+  components: {
+    ButtonNew
+  },
   data() {
     return {
       src: `${URL.WP}/static/_id/natsume.jpg`,
@@ -32,6 +39,10 @@ export default {
   padding: 0 50px;
   width: 100%;
   height: 100%;
+  @include pc {
+    margin: 0 auto;
+    width: 800px;
+  }
   .head {
     font-size: 25px;
     font-weight: bold;
@@ -49,14 +60,8 @@ export default {
     font-size: 12px;
     line-height: 2;
   }
-  .email {
+  .ButtonNew {
     margin-top: 30px;
-    padding: 0 20px;
-    font-size: 14px;
-    font-weight: bold;
-    line-height: 50px;
-    background: #121212;
-    border-radius: 25px;
   }
 }
 </style>
