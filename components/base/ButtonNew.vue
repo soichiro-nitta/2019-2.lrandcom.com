703e596695b7a0785225e70bcd3146197b044e9c
soichiro-nitta <soic1226@gmail.com>
2019-01-11 16:01:58 +0900

    add ButtonNew component

---
+++ components/base/ButtonNew.vue
@@ -0,0 +1,37 @@
+<template>
+  <a
+    :href="to"
+    class="ButtonNew"
+    target="_blank"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.ButtonNew {
+  display: block;
+  width: 250px;
+  line-height: 50px;
+  background: #121212;
+  border-radius: 25px;
+  font-weight: bold;
+  font-size: 14px;
+  color: white;
+}
+</style>
