803d52f4d1f2b3947912adfc140c383ebfdb6fd5
soichiro-nitta <soic1226@gmail.com>
2019-01-22 05:35:45 +0900

    update transition

--- pages/website.vue
+++ pages/website.vue
@@ -23,11 +23,11 @@ export default {
     document.getElementById('scrollArea').scrollTop = 0
   },
   async beforeRouteLeave(to, from, next) {
-    const scrollArea = document.getElementById('scrollArea')
-    TweenMax.to(scrollArea, 0.7, {
-      scrollTop: scrollArea.scrollTop - 100,
-      ease: Expo.easeOut
-    })
+    // const scrollArea = document.getElementById('scrollArea')
+    // TweenMax.to(scrollArea, 0.7, {
+    //   scrollTop: scrollArea.scrollTop - 100,
+    //   ease: Expo.easeOut
+    // })
     this.toggleLeave()
     await this.$delay(700)
     next()
