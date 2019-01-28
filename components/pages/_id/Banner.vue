2ebe69437a8d3edccf71ca4b8c778364d174f531
soichiro-nitta <soic1226@gmail.com>
2019-01-28 18:08:54 +0900

    update Banner

--- components/pages/_id/Banner.vue
+++ components/pages/_id/Banner.vue
@@ -1,37 +1,64 @@
 <template>
-  <div
-    class="Banner"
-  >
-    <div class="BannerInsta"/>
-    <!-- <div class="head">Instagramやっています！</div>
-    <div class="text">リーディング＆カンパニーのInstagramでは、世の中のある瞬間をとらえた写真や、イラストを日々アップしています。記事と合わせて、こちらも逃さずチェック!!</div>
-    <ButtonNew
+  <div class="Banner">
+    <BannerItemNew
+      v-if="num === 1"
+      :num="num"
       :to="insta"
-      text="LCのInstagramをみる"
-    /> -->
+      head="インスタ毎日更新中！!"
+      body="iPhone時代だからこそ、何を撮るかが問われる。その人にしか美しく見えないもの。"
+      button="Instagramをみる"
+      @increment="increment"
+    />
+    <BannerItemNuxt
+      v-if="num === 2"
+      :num="num"
+      to="/writer"
+      head="ライター募集中！!"
+      body="書き手がつまらないと感じたら、読み手はその10倍はつまらないよね。"
+      button="詳細をみる"
+      @increment="increment"
+    />
   </div>
 </template>
 
 <script>
 import URL from '~/assets/data/url.json'
 import { TweenMax, Expo } from 'gsap'
-import ButtonNew from '~/components/base/ButtonNew'
+import BannerItemNew from './BannerItemNew'
+import BannerItemNuxt from './BannerItemNuxt'
 
 export default {
   components: {
-    ButtonNew
+    BannerItemNew,
+    BannerItemNuxt
   },
   data() {
     return {
-      insta: URL.INSTA
+      insta: URL.INSTA,
+      num: 1
     }
   },
-  mounted() {}
+  mounted() {},
+  methods: {
+    async increment() {
+      await this.$delay(1000)
+      this.num++
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .Banner {
+  .BannerWriter {
+    position: fixed;
+    bottom: 30px;
+    left: 30px;
+    padding: 30px;
+    width: calc(100% - 60px);
+    background: white;
+    opacity: 0;
+  }
   .BannerInsta {
     position: fixed;
     bottom: 30px;
@@ -39,28 +66,7 @@ export default {
     padding: 30px;
     width: calc(100% - 60px);
     background: #171717;
+    opacity: 0;
   }
-  // margin: 30px auto 0;
-  // padding: 30px;
-  // width: calc(100% - 60px);
-  // text-align: center;
-  // background: #171717;
-  // border-radius: 20px;
-  // @include pc {
-  //   width: 540px;
-  // }
-  // .head {
-  //   font-size: 14px;
-  //   font-weight: bold;
-  // }
-  // .text {
-  //   margin: 20px auto 0;
-  //   font-size: 12px;
-  //   color: #999;
-  //   line-height: 2;
-  // }
-  // .ButtonNew {
-  //   margin: 20px auto 0;
-  // }
 }
 </style>
