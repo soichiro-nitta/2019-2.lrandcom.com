14919180dff51e2bf60ac8ba644d38d3805321eb
soichiro-nitta <soic1226@gmail.com>
2019-02-02 19:39:27 +0900

    clean default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,59 @@
 <template>
-  <Layout/>
+  <div class="Layout">
+    <Background/>
+    <nuxt/>
+    <!-- <no-ssr v-if="$device.isMobile">
+      <Burger
+        :opening="opening"
+        :menu="menu"
+        @openMenu="openMenu"
+      />
+    </no-ssr> -->
+    <!-- <Logo :opening="opening"/> -->
+    <!-- <no-ssr v-if="$device.isMobile">
+      <Shade
+        :menu="menu"
+        @closeMenu="closeMenu"
+      />
+    </no-ssr> -->
+    <Menu
+      :menu="menu"
+      :opening="opening"
+      @closeMenu="closeMenu"
+    />
+    <Opening/>
+  </div>
 </template>
 
 <script>
-import Layout from '~/components/layouts/default/Layout'
+import { mapGetters, mapMutations } from 'vuex'
+import Background from '~/components/default/Background'
+import Logo from '~/components/default/Logo'
+import Burger from '~/components/default/Burger'
+import Shade from '~/components/default/Shade'
+import Menu from '~/components/default/Menu'
+import Opening from '~/components/default/Opening'
 
 export default {
   components: {
-    Layout
+    Background,
+    Logo,
+    Burger,
+    Shade,
+    Menu,
+    Opening
+  },
+  computed: {
+    ...mapGetters({
+      opening: 'opening',
+      menu: 'menu'
+    })
+  },
+  methods: {
+    ...mapMutations({
+      openMenu: 'openMenu',
+      closeMenu: 'closeMenu'
+    })
   }
 }
 </script>
