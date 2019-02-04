e2c1961c67f20abaa876499c62e2153a9f656557
soichiro-nitta <soic1226@gmail.com>
2019-02-04 15:33:03 +0900

    update opening animation

--- components/default/Opening.vue
+++ components/default/Opening.vue
@@ -4,6 +4,20 @@
     ref="opening"
     class="Opening"
   >
+    <div class="shapes">
+      <div class="shape shape1 whitegray" />
+      <div class="shape shape1 gray" />
+      <div class="shape shape1 black" />
+      <div class="shape shape2 whitegray" />
+      <div class="shape shape2 gray" />
+      <div class="shape shape2 black" />
+      <div class="shape shape3 whitegray" />
+      <div class="shape shape3 gray" />
+      <div class="shape shape3 black" />
+      <div class="shape shape4 whitegray" />
+      <div class="shape shape4 gray" />
+      <div class="shape shape4 black" />
+    </div>
     <div
       ref="lottie"
       class="lottie"
@@ -25,20 +39,32 @@ export default {
     const anim = lottie.loadAnimation({
       container: this.$refs.lottie,
       renderer: 'svg',
+      autoplay: false,
       loop: false,
       path: '/json/icon.json'
     })
 
     await this.$loadWindow()
     await this.$raf()
-    TweenMax.to(document.body, 1.7, {
-      backgroundColor: 'white',
-      ease: Expo.easeInOut
-    })
-    anim.setSpeed(1.15)
+    this.shapeIn()
+
+    await this.$delay(750)
+    anim.setSpeed(1.1)
     anim.play() // 再生
 
     await this.$completeLottie(anim)
+    TweenMax.to(this.$refs.lottie, 0.02, {
+      opacity: 0.5,
+      startAt: {
+        opacity: 1
+      },
+      ease: Expo.easeOut,
+      repeat: 7,
+      yoyo: true,
+      repeatDelay: 0.03
+    })
+
+    await this.$delay(500)
     anim.setDirection(-1)
     anim.setSpeed(1.5)
 
@@ -47,15 +73,113 @@ export default {
 
     await this.$delay(250)
     await this.$raf()
-    this.$emit('open') // ストア変更
-    TweenMax.to(this.$refs.opening, 3, {
+    TweenMax.to(this.$refs.lottie, 2.4, {
       scale: 4,
-      opacity: 0,
       ease: Expo.easeOut
     })
 
-    await this.$delay(3000)
+    await this.$delay(500)
+    this.shapeOut()
+
+    await this.$delay(250)
+    this.$emit('open') // ストア変更
+
+    await this.$delay(1650)
     this.hide = true
+  },
+  methods: {
+    shapeIn() {
+      TweenMax.to('.shapes', 1.9, {
+        opacity: 1,
+        scale: 1,
+        ease: Expo.easeOut
+      })
+      TweenMax.staggerTo(
+        '.shape1',
+        0.3,
+        {
+          y: '-100%',
+          ease: Expo.easeInOut
+        },
+        0.2
+      )
+      TweenMax.staggerTo(
+        '.shape2',
+        0.3,
+        {
+          y: '100%',
+          ease: Expo.easeInOut,
+          delay: 0.2
+        },
+        0.2
+      )
+      TweenMax.staggerTo(
+        '.shape3',
+        0.3,
+        {
+          y: '-100%',
+          ease: Expo.easeInOut,
+          delay: 0.4
+        },
+        0.2
+      )
+      TweenMax.staggerTo(
+        '.shape4',
+        0.3,
+        {
+          y: '100%',
+          ease: Expo.easeInOut,
+          delay: 0.6
+        },
+        0.2
+      )
+    },
+    shapeOut() {
+      TweenMax.to('.shapes', 1.9, {
+        opacity: 0,
+        scale: 2,
+        ease: Expo.easeInOut
+      })
+      TweenMax.staggerTo(
+        [].slice.call(document.getElementsByClassName('shape4')).reverse(),
+        0.3,
+        {
+          y: '0%',
+          ease: Expo.easeInOut
+        },
+        0.2
+      )
+      TweenMax.staggerTo(
+        [].slice.call(document.getElementsByClassName('shape3')).reverse(),
+        0.3,
+        {
+          y: '0%',
+          ease: Expo.easeInOut,
+          delay: 0.2
+        },
+        0.2
+      )
+      TweenMax.staggerTo(
+        [].slice.call(document.getElementsByClassName('shape2')).reverse(),
+        0.3,
+        {
+          y: '0%',
+          ease: Expo.easeInOut,
+          delay: 0.4
+        },
+        0.2
+      )
+      TweenMax.staggerTo(
+        [].slice.call(document.getElementsByClassName('shape1')).reverse(),
+        0.3,
+        {
+          y: '0%',
+          ease: Expo.easeInOut,
+          delay: 0.6
+        },
+        0.2
+      )
+    }
   }
 }
 </script>
@@ -70,13 +194,69 @@ export default {
   right: 0;
   bottom: 0;
   left: 0;
+  // top: 5px;
+  // right: 5px;
+  // bottom: 5px;
+  // left: 5px;
   margin: auto;
-  padding: 0 40%;
   width: 100%;
   height: 100%;
-  background: black;
-  @include pc {
-    padding: 0 45%;
+  // width: calc(100% - 10px);
+  // height: calc(100% - 10px);
+  overflow: hidden;
+  // background: black;
+  .lottie {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    padding: 0 40%;
+  }
+  .shapes {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    opacity: 0;
+    transform: scale(2);
+    .shape {
+      position: absolute;
+      width: 25%;
+      height: 100%;
+    }
+    .shape1 {
+      bottom: -100%;
+      left: 0;
+      margin: auto;
+    }
+    .shape2 {
+      top: -100%;
+      left: 25%;
+      margin: auto;
+    }
+    .shape3 {
+      bottom: -100%;
+      left: 50%;
+      margin: auto;
+    }
+    .shape4 {
+      top: -100%;
+      left: 75%;
+      margin: auto;
+    }
+    .whitegray {
+      background: #aaa;
+    }
+    .gray {
+      background: #555;
+    }
+    .black {
+      background: #000;
+    }
   }
 }
 </style>
