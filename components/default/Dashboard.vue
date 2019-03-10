8064c2536e5ab89a7432cdc16b823320d32f0516
soichiro-nitta <soic1226@gmail.com>
2019-03-10 14:56:34 +0900

    add: Dashboardコンポーネントを追加

---
+++ components/default/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Dashboard">
+    Dashboard
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.Dashboard {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 300px;
+  height: 100%;
+  background: white;
+}
+</style>
